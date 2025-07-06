c86aee4b37d78418ad1d016b682c53757f3ce751
Joe Alongi <email@heyitsjoealongi.com>
2025-07-06 18:03:39 -0400

    Updated

--- pinpointscore/src/functions/request.ts
+++ pinpointscore/src/functions/request.ts
@@ -1,31 +1,3 @@
-// import axios from "axios";
-// import { encrypt, decrypt } from "./security";
-
-// export const postRequest = async (endpoint: string, body: unknown) => {
-//   try {
-//     const base = import.meta.env.VITE_BASE_API_URL ?? "";
-//     const proxy = import.meta.env.VITE_PROXY_URL ?? "";
-//     const payload = { base: base, endpoint: endpoint, body: body };
-//     const encrypted = await encrypt(payload);
-//     if (!encrypted) {
-//       throw new Error("Encryption failed");
-//     }
-//     const ciphertext = encrypted;
-//     const decrypted_content = await decrypt(ciphertext);
-//     console.log("-----ciphertext", ciphertext);
-//     console.log("-----decrypted_content", decrypted_content);
-//     const { data } = await axios.post(proxy, ciphertext, {
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     });
-//     return data;
-//   } catch (error) {
-//     console.log("Error posting request", error);
-//     return error;
-//   }
-// };
-
 import axios from "axios";
 
 import { encrypt, envelope } from "./security";
--- pinpointscorecourse/utils/security.ts
+++ pinpointscorecourse/utils/security.ts
@@ -1,14 +1,12 @@
 export const decrypt = async (ciphertext: ArrayBuffer): Promise<any> => {
   try {
-    const pemPrivateKey = process.env.PRIVATE_KEY ?? "";
-    const header = "-----BEGIN PRIVATE KEY-----";
-    const footer = "-----END PRIVATE KEY-----";
-    const contents = pemPrivateKey
-      .replace(header, "")
-      .replace(footer, "")
+    const ppk = process.env.PRIVATE_KEY ?? "";
+    const contents = ppk
+      .replace("-----BEGIN PRIVATE KEY-----", "")
+      .replace("-----END PRIVATE KEY-----", "")
       .replace(/\s/g, "");
-    const binary = Uint8Array.from(atob(contents), (c) => c.charCodeAt(0));
-    const privateKey = await crypto.subtle.importKey(
+    const binary = Uint8Array?.from(atob(contents), (c) => c?.charCodeAt(0));
+    const privateKey = await crypto?.subtle?.importKey(
       "pkcs8",
       binary.buffer,
       {
@@ -18,14 +16,14 @@ export const decrypt = async (ciphertext: ArrayBuffer): Promise<any> => {
       false,
       ["decrypt"]
     );
-    const decrypted = await crypto.subtle.decrypt(
+    const decrypted = await crypto?.subtle?.decrypt(
       { name: "RSA-OAEP" },
       privateKey,
       ciphertext
     );
     const decoder = new TextDecoder();
-    const decoded = decoder.decode(decrypted);
-    return JSON.parse(decoded);
+    const decoded = decoder?.decode(decrypted);
+    return JSON?.parse(decoded);
   } catch (error) {
     console.error("Error decrypting data", error);
   }
