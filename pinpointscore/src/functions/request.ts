94ed79caf9fb3e03b8be2a891095c0f874775f30
Joe Alongi <email@heyitsjoealongi.com>
2025-07-06 16:27:17 -0400

    Encryption From Keys

--- pinpointscore/src/functions/request.ts
+++ pinpointscore/src/functions/request.ts
@@ -10,8 +10,8 @@ export const postRequest = async (endpoint: string, body: unknown) => {
     if (!encrypted) {
       throw new Error("Encryption failed");
     }
-    const { ciphertext, counter, key } = encrypted;
-    const decrypted_content = await decrypt(ciphertext, counter, key);
+    const ciphertext = encrypted;
+    const decrypted_content = await decrypt(ciphertext);
     console.log("-----ciphertext", ciphertext);
     console.log("-----decrypted_content", decrypted_content);
     const { data } = await axios.post(proxy, ciphertext, {
@@ -25,3 +25,25 @@ export const postRequest = async (endpoint: string, body: unknown) => {
     return error;
   }
 };
+
+// import axios from "axios";
+
+// import { encrypt } from "./security";
+
+// export const postRequest = async (endpoint: string, body: unknown) => {
+//   try {
+//     const base = import.meta.env.VITE_BASE_API_URL ?? "";
+//     const proxy = import.meta.env.VITE_PROXY_URL ?? "";
+//     const payload = { base: base, endpoint: endpoint, body: body };
+//     const encrypted = await encrypt(payload);
+//     const { data } = await axios.post(proxy, encrypted, {
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     console.log("Error posting request", error);
+//     return error;
+//   }
+// };
--- pinpointscore/src/functions/security.ts
+++ pinpointscore/src/functions/security.ts
@@ -1,51 +1,115 @@
-export const encrypt = async (payload: object) => {
+export const encrypt = async (
+  payload: object
+): Promise<ArrayBuffer | undefined> => {
   try {
-    const key = await window.crypto.subtle.generateKey(
+    const pemPublicKey = import.meta.env.VITE_PUBLIC_KEY ?? "";
+    const pemHeader = "-----BEGIN PUBLIC KEY-----";
+    const pemFooter = "-----END PUBLIC KEY-----";
+    const pemContents = pemPublicKey
+      .replace(pemHeader, "")
+      .replace(pemFooter, "")
+      .replace(/\s/g, "");
+    const binaryDer = Uint8Array.from(window.atob(pemContents), (c) =>
+      c.charCodeAt(0)
+    );
+    const publicKey = await window.crypto.subtle.importKey(
+      "spki",
+      binaryDer.buffer,
       {
-        name: "AES-CTR",
-        length: 256,
+        name: "RSA-OAEP",
+        hash: "SHA-256",
+      },
+      false,
+      ["encrypt"]
+    );
+    const encoder = new TextEncoder();
+    const encoded = encoder.encode(JSON.stringify(payload));
+    const ciphertext = await window.crypto.subtle.encrypt(
+      { name: "RSA-OAEP" },
+      publicKey,
+      encoded
+    );
+    return ciphertext;
+  } catch (error) {
+    console.error("Error encrypting data", error);
+  }
+};
+
+export const decrypt = async (ciphertext: ArrayBuffer): Promise<any> => {
+  try {
+    const pemPrivateKey = import.meta.env.VITE_PRIVATE_KEY ?? "";
+    const pemHeader = "-----BEGIN PRIVATE KEY-----";
+    const pemFooter = "-----END PRIVATE KEY-----";
+    const pemContents = pemPrivateKey
+      .replace(pemHeader, "")
+      .replace(pemFooter, "")
+      .replace(/\s/g, "");
+    const binaryDer = Uint8Array.from(window.atob(pemContents), (c) =>
+      c.charCodeAt(0)
+    );
+    const privateKey = await window.crypto.subtle.importKey(
+      "pkcs8",
+      binaryDer.buffer,
+      {
+        name: "RSA-OAEP",
+        hash: "SHA-256",
+      },
+      false,
+      ["decrypt"]
+    );
+    const decrypted = await window.crypto.subtle.decrypt(
+      { name: "RSA-OAEP" },
+      privateKey,
+      ciphertext
+    );
+    const decoder = new TextDecoder();
+    const decoded = decoder.decode(decrypted);
+    return JSON.parse(decoded);
+  } catch (error) {
+    console.error("Error decrypting data", error);
+  }
+};
+
+export const generate = async () => {
+  try {
+    const keyChain = { public: "", private: "" };
+    const keyPair = await window.crypto.subtle.generateKey(
+      {
+        name: "RSA-OAEP",
+        modulusLength: 4096,
+        publicExponent: new Uint8Array([1, 0, 1]),
+        hash: "SHA-256",
       },
       true,
       ["encrypt", "decrypt"]
     );
-    const counter = window.crypto.getRandomValues(new Uint8Array(16));
-    const encoder = new TextEncoder();
-    const json = JSON.stringify(payload);
-    const data = encoder.encode(json);
-    const ciphertext = await window.crypto.subtle.encrypt(
-      {
-        name: "AES-CTR",
-        counter,
-        length: 64,
-      },
-      key,
-      data
-    );
-    return { ciphertext, counter, key };
-  } catch (error) {
-    console.log("Error encrypting data", error);
-  }
-};
 
-export const decrypt = async (
-  ciphertext: ArrayBuffer,
-  counter: Uint8Array,
-  key: CryptoKey
-) => {
-  try {
-    const decrypted = await window.crypto.subtle.decrypt(
-      {
-        name: "AES-CTR",
-        counter,
-        length: 64,
-      },
-      key,
-      ciphertext
+    // Extract Public
+    const exportedPublic = await window.crypto.subtle.exportKey(
+      "spki",
+      keyPair.publicKey
     );
-    const decoder = new TextDecoder();
-    const json = decoder.decode(decrypted);
-    return JSON.parse(json);
+    const exportedPublicAsString = String.fromCharCode(
+      ...new Uint8Array(exportedPublic)
+    );
+    const exportedPublicAsBase64 = window.btoa(exportedPublicAsString);
+    keyChain["public"] =
+      `-----BEGIN PUBLIC KEY-----\n${exportedPublicAsBase64}\n-----END PUBLIC KEY-----`;
+
+    // Extract Private
+    const exportedPrivate = await window.crypto.subtle.exportKey(
+      "pkcs8",
+      keyPair.privateKey
+    );
+    const exportedPrivateAsString = String.fromCharCode(
+      ...new Uint8Array(exportedPrivate)
+    );
+    const exportedPrivateAsBase64 = window.btoa(exportedPrivateAsString);
+    keyChain["private"] =
+      `-----BEGIN PRIVATE KEY-----\n${exportedPrivateAsBase64}\n-----END PRIVATE KEY-----`;
+
+    return keyChain;
   } catch (error) {
-    console.log("Error decrypting data", error);
+    console.error("Error generating keys", error);
   }
 };
